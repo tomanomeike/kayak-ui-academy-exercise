2727e2fd17cc1d287d170765edcb3c95ef4d5ab6
LAPTOP-D77I3KMJ\toman <tomanom@gmail.com>
2019-02-24 15:06:44 +0200

    search in console

--- src/components/autocomplete/autocomplete.js
+++ src/components/autocomplete/autocomplete.js
@@ -1,72 +1,41 @@
 import React, { Component } from 'react';
 
+import $ from 'jquery';
 import styles from './autocomplete.css';
 
 class Autocomplete extends Component {
   constructor(props) {
     super(props);
     this.state = {};
-    // console.log('Tai mano');
-
-    // const movies = [
-    //   { id: 0, title: 'Averenger', overview: 'jhjhjugjg' },
-    //   { id: 1, title: 'Averengerdgdg', overview: 'jhjhjugjjgfkjfgkg' }
-    // ];
-    //
-    // this.state = {
-    //   rows: [
-    //     <p key="1">This is my row1</p>,
-    //     <p key="2">This is my row2</p>,
-    //     <p key="3">This is my row3</p>
-    //   ]
-    // };
-    // const movieRows = [];
-    // movies.forEach(movie => {
-    //   console.log(movie.id);
-    //   movieRows.push(
-    //     <p key={movie.id}>
-    //       movie title:
-    //       {movie.title}
-    //     </p>
-    //   );
-    // });
-    // this.state = { rows: movieRows };
-    this.performSearch();
+    this.performSearch('ant man');
   }
   performSearch(searchTerm) {
-    // const urlString = `https://api.themoviedb.org/3/search/movie?api_key=cab2afe8b43cf5386e374c47aeef4fca&language=en-US&query={paieskos raktas}&page=1&include_adult=false`;
+    // const urlString = `https://api.themoviedb.org/3/search/movie?api_key=cab2afe8b43cf5386e374c47aeef4fca&language=en-US&query=marvel&page=1&include_adult=false`;
 
     const urlString =
       'https://api.themoviedb.org/3/search/movie?' +
       `api_key=cab2afe8b43cf5386e374c47aeef4fca` +
       `&language=en-US&query=${searchTerm}` +
       '&page=1&include_adult=false';
-    // ajax({
-    //   url: urlString,
-    //   success: searchResults => {
-    //     console.log('tikrai');
-    //   },
-    //   error: (xhr, status, err) => {
-    //     console.error('failed');
-    //   }
-    // });
-    fetch(urlString)
-      .then(response => response.json())
-      .then(responseJson => {
-        return responseJson.movies;
-        const results = searchResults.results;
 
-        const movieRows = [];
+    $.ajax({
+      url: urlString,
+      success: searchResults => {
+        console.log('tikrai');
+        console.log(searchResults);
+        const results = searchResults.results;
+        // console.log (results [0])
+        const movieRow = [];
 
         results.forEach(movie => {
-          const movieRow = <MovieRow key={movie.id} movie={movie} />;
+          const movieRows = <MovieRow key={movie.id} movie={movie} />;
           movieRows.push(movie);
         });
-        this.setState({ rows: movieRows });
-      })
-      .catch(error => {
-        console.error(error);
-      });
+      },
+      error: (xhr, status, err) => {
+        console.error('failed');
+      }
+    });
   }
 
   searchChangeHandler(event) {
