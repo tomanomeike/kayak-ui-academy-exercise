82e46f7b3b5c9bbd4b70215ff79c06cabde06d21
LAPTOP-D77I3KMJ\toman <tomanom@gmail.com>
2019-02-27 13:25:47 +0200

    show results

--- src/components/autocomplete/autocomplete.js
+++ src/components/autocomplete/autocomplete.js
@@ -3,16 +3,14 @@ import React, { Component } from 'react';
 import $ from 'jquery';
 import styles from './autocomplete.css';
 
-
 class Autocomplete extends Component {
   constructor(props) {
     super(props);
     this.state = {};
-    this.performSearch('ant man');
+    this.performSearch();
   }
-  performSearch(searchTerm) {
-    // const urlString = `https://api.themoviedb.org/3/search/movie?api_key=cab2afe8b43cf5386e374c47aeef4fca&language=en-US&query=marvel&page=1&include_adult=false`;
 
+  performSearch(searchTerm) {
     const urlString =
       'https://api.themoviedb.org/3/search/movie?' +
       `api_key=cab2afe8b43cf5386e374c47aeef4fca` +
@@ -25,13 +23,17 @@ class Autocomplete extends Component {
         console.log('tikrai');
         console.log(searchResults);
         const results = searchResults.results;
-        // console.log (results [0])
-        const movieRow = [];
 
+        const movieRows = [];
         results.forEach(movie => {
-          const movieRows = <MovieRow key={movie.id} movie={movie} />;
-          movieRows.push(movie);
+          movieRows.push(
+            <table>
+              <h3 key={movie.id}>{movie.title}</h3>
+            </table>
+          );
         });
+        this.state = { movieRows };
+        this.setState({ rows: movieRows });
       },
       error: (xhr, status, err) => {
         console.error('failed');
@@ -45,21 +47,20 @@ class Autocomplete extends Component {
     const searchTerm = event.target.value;
     boundObject.performSearch(searchTerm);
   }
+
   render() {
     return (
-
       <div className={styles.container}>
         <div className={styles.wrapper}>
           <div className={styles.menu}>
             <div className={styles.logo}>
-                <img src="film.png"/>
+              <img width="50" src="search.png" />
             </div>
             <input onChange={this.searchChangeHandler.bind(this)} placeholder="Enter movie name" />
-            <div className={styles.search}>
-              <img width="20px" src="search.png" />
-            </div>
-            {this.state.rows}
+
+            <div className={styles.search}>{<img width="50" src="search.png" />}</div>
           </div>
+          {this.state.rows}
         </div>
       </div>
     );
